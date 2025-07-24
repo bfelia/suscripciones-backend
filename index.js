5f0ecc72b2334f76b846c79c9aea4d9f61b60316
Bautista <bautistafernandezmelia@gmail.com>
2025-07-24 13:06:47 -0300

    subida a render6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 import { MercadoPagoConfig, PreApproval } from "mercadopago";
 import dotenv from "dotenv";
-import webhookRouter from "./webhook.js"
+import webhookRouter from "./webhook.js";
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ const config = new MercadoPagoConfig({
   accessToken: process.env.ACCESS_TOKEN, // o tu token directo
 });
 
-const preaproval = new PreApproval(config);
+const preapproval = new PreApproval(config);
 
 app.post("/crear-suscripcion", async (req, res) => {
   try {
@@ -37,7 +37,7 @@ app.post("/crear-suscripcion", async (req, res) => {
     };
     
     console.log("req.body recibido:", req.body);
-    const newSuscriber = await preaproval.create({ body });
+    const newSuscriber = await preapproval.create({ body });
 
     console.log("✅ Suscripción creada:", newSuscriber);
     res.status(200).json({ init_point: newSuscriber.init_point });
@@ -47,7 +47,7 @@ app.post("/crear-suscripcion", async (req, res) => {
   }
 });
 
-app.use("/", webhookRouter);
+app.use("/", webhookRouter(preapproval));
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
