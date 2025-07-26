3fc1c49906cc8b8934c8f1161c22659005b87264
Bautista <bautistafernandezmelia@gmail.com>
2025-07-26 00:29:44 -0300

    subida a render21

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 import { MercadoPagoConfig, PreApproval } from "mercadopago";
 import dotenv from "dotenv";
 import webhookRouter from "./webhook.js";
+import { db } from "./firebase.js"; // o tu archivo donde inicializás Firebase
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,10 +17,32 @@ const config = new MercadoPagoConfig({
 
 const preapproval = new PreApproval(config);
 
+const tieneSuscripcionActiva = async (userId, barberiaId) => {
+  try {
+    const suscripcionesRef = db.collection('suscripciones_activas').doc(userId);
+    const snapshot = await suscripcionesRef
+      .where('barberiaId', '==', barberiaId)
+      .where('status', '==', 'authorized')
+      .get();
+
+    return !snapshot.empty; // true si hay una activa
+  } catch (error) {
+    console.error('Error al verificar suscripción activa:', error);
+    return false;
+  }
+};
+
+
 app.post("/crear-suscripcion", async (req, res) => {
   try {
 
     const { userId, barberiaId, planId, cortesPlan, monto, userEmail, nombreUsuario } = req.body;
+
+    const yaSuscripto = await tieneSuscripcionActiva(userId);
+
+    if (yaSuscripto) {
+      return res.status(400).json({ error: 'El usuario ya tiene una suscripción activa.' });
+    }
 
     const body = {
       reason: `${planId}_${cortesPlan}-user_${userId}_${nombreUsuario}-barberia_${barberiaId}`,
@@ -36,6 +59,10 @@ app.post("/crear-suscripcion", async (req, res) => {
       payer_email: "test_user_491019957@testuser.com", // Esto se reemplaza luego por el email real 
       external_reference: barberiaId,
     };
+
+    if (!userId || !barberiaId || !planId || !cortesPlan || !monto || !userEmail || !nombreUsuario) {
+      return res.status(400).json({ error: 'Faltan datos necesarios para crear la suscripción.' });
+    }
 
     console.log("req.body recibido:", req.body);
     const newSuscriber = await preapproval.create({ body });
